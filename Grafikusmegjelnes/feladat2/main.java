8e2434f67a5daa7996773c81e3a9b95b9b777d87
tepi7 <tepi7@Tepi>
2024-02-24 14:00:49 +0100

    Vizsga feladatok feltoltese

---
+++ Grafikusmegjelnes/feladat2/main.java
@@ -0,0 +1,31 @@
+package feladat2;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		euroszamla bankszamla2=new euroszamla();
+		forintszamla bankszamla = new forintszamla();
+		
+		bankszamla.setForintszamal(true);
+		bankszamla.setEgyenleg(5000);
+		bankszamla.befizetes(5000);
+		bankszamla.kifizetes(500);
+		bankszamla.setNev("Andras");
+		bankszamla.setJelszo(456);
+		bankszamla.getEgyenleg();
+		
+		bankszamla2.setNev("Peter");
+		bankszamla2.setJelszo(123);
+		bankszamla2.setEuroszamal(true);
+		bankszamla2.setEgyenleg(6000);
+		bankszamla2.getEgyenleg();
+		bankszamla2.kifizetes(105);
+		
+		
+	}
+
+}
---
+++ Grafikusmegjelnes/feladat3/main.java
@@ -0,0 +1,19 @@
+package feladat3;
+/*
+ * rögzitve van a nev,osztaly,testsuly,eletkor,sportag
+ * adatok rögzitese
+ * csak a megadott adatokat kerje
+ * konstansként legyen valaszthato sportag
+ * a bevitt adatok megjelenitese
+ * lehessen listázni minimum 2 szempont(sportag,eletkor), valamint új adat felvetele
+ * 
+ * 
+ */
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
